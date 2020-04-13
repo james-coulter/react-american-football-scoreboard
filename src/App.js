c17d8accd0c620ebbcd061908dfd741037ec562b
Christian Coulter <christiancoulter@Christians-MacBook-Air-2.local>
2020-04-13 09:53:11 -0600

    Finished Steps 1 and 2. Working on onClick listeners next.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
+
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [home, updateScoreH] = useState(0)
+
+  const tdHome = e => {
+    updateScoreH(home + 7)
+  }
+
+  const fgHome = e => {
+    updateScoreH(home + 3)
+  }
+
+  const [away, updateScoreA] = useState(0)
+
+  const tdAway = e => {
+    updateScoreA(away + 7)
+  }
+
+  const fgAway = e => {
+    updateScoreA(away + 3)
+  }
 
   return (
     <div className="container">
@@ -14,12 +34,12 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
+            <div className="home__score">{home}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{away}</div>
           </div>
         </div>
         <BottomRow />
@@ -28,7 +48,7 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick= {tdHome} className="homeButtons__touchdown">Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
